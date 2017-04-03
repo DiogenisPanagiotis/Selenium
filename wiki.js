eb94534881477b6cd1a375485eedd3d50a18d0ea
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-04-03 11:54:02 -0400

    Add wiki script.

---
+++ wiki.js
@@ -0,0 +1,10 @@
+const webdriver = require('selenium-webdriver');
+const browser = new webdriver.Builder() // build WebDriver
+    .forBrowser('chrome') // for Chrome
+    .build();
+
+browser.get('http://en.wikipedia.org/wiki/Wiki');
+browser.findElements(webdriver.By.css('[href^="/wiki/"]')).then((links)=>{
+    console.log('Found', links.length, 'Wiki links.');
+    browser.quit();
+});
