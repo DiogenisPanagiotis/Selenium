0358ec1b45fbb5239567e915b6e545701a66b933
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-04-03 12:45:13 -0400

    Fix format.

--- wiki.js
+++ wiki.js
@@ -4,7 +4,7 @@ const browser = new webdriver.Builder() // build WebDriver
      .build();
 
 browser.get('http://en.wikipedia.org/wiki/Wiki');
-browser.findElements(webdriver.By.css('[href^="/wiki/"]')).then((links)=>{
-    console.log('Found', links.length, 'Wiki links.');
-    browser.quit();
+browser.findElements(webdriver.By.css('[href^="/wiki/"]')).then((links) => {
+  console.log('Found', links.length, 'Wiki links.');
+  browser.quit();
 });
