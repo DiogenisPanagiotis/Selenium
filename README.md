ee0f8b6865fd7b955b1ede99401c5b2ee2ee85fe
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-04-03 10:49:34 -0400

    Add es6.

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Example Selenium 2 WebDriver scripts using JavaScript.
 ![](https://image.ibb.co/iF2u0a/ladybug.jpg)
 
 ## Usage
-npm install selenium-webdriver
+ - npm install selenium-webdriver
+ - npm install chromedriver
 
 ## Download
-http://www.seleniumhq.org/download/
+(Selenium) - http://www.seleniumhq.org/download/
+(ChromeDriver) - https://sites.google.com/a/chromium.org/chromedriver/downloads
 
 ## Documentation
 http://www.seleniumhq.org/docs/
--- cheese.js
+++ cheese.js
@@ -1,21 +1,25 @@
-var driver = new webdriver.Builder().build(); // build WebDriver
+const webdriver = require('selenium-webdriver');
+const driver = new webdriver.Builder() // build WebDriver
+    .forBrowser('chrome') // for Chrome
+    .build();
+
 driver.get('http://www.google.com'); // fetch url
 
-var element = driver.findElement(webdriver.By.name('q'));
+let element = driver.findElement(webdriver.By.name('q'));
 element.sendKeys('Cheese!');
 element.submit();
 
-driver.getTitle().then(function(title) {
+driver.getTitle().then((title) => {
   console.log(`Page title is: ${title}`);
 });
 
-driver.wait(function() {
+driver.wait(() => {
   return driver.getTitle().then(function(title) {
     return title.toLowerCase().lastIndexOf('cheese!', 0) === 0;
   });
 }, 3000);
 
-driver.getTitle().then(function(title) {
+driver.getTitle().then((title) => {
   console.log(`Page title is: ${title}`);
 });
 
